91868bb76fcfef3aea67ca11b1ae26898bba7dbd
YourName <noor11809027@gmail.com>
2022-09-02 14:34:47 +0600

    components added

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
-const app=Vue.createApp({
-    data(){
-        return{
-            friends:[
-                {
-                    id:"noor9027",
-                    name:"Noor Mohammad Talukder",
-                    phone:"321 654 987",
-                    email:"abc@gmail.com",
-                },
-                {
-                    id:"alif724",
-                    name:"Alif Islam",
-                    phone:"123 456 789",
-                    email:"bbc@gmail.com",
-                },
-            ]
-        }
-    }
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: "noor9027",
+          name: "Noor Mohammad Talukder",
+          phone: "321 654 987",
+          email: "abc@gmail.com",
+        },
+        {
+          id: "alif724",
+          name: "Alif Islam",
+          phone: "123 456 789",
+          email: "bbc@gmail.com",
+        },
+      ],
+    };
+  },
 });
-app.component('friend-contact',{
-    data(){
-        return{
-            detailsAreVisible:false,
-        }
+
+app.component("friend-contact", {
+  template: `
+    <li >
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails()">
+        {{detailsAreVisible? 'Hide':'Show'}} Details</button>
+
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{friend.phone}}</li>
+            <li><strong>Email:</strong>{{friend.email}}</li>
+        </ul>
+    </li>
+    `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "noor9027",
+        name: "Noor Mohammad Talukder",
+        phone: "321 654 987",
+        email: "abc@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
     },
-    methods:{
-        toggleDetails(){
-            this.detailsAreVisible=!this.detailsAreVisible;
-        }
-    }
-})
-app.mount('#app');
+  },
+});
+app.mount("#app");
